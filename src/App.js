9d938f95aa6bba7febefb1abee11afebf32fbe75
Mary Bond <mbond1224@gmail.com>
2018-07-10 22:55:18 -0700

    added starter files

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
 
-class ContactList extends Component{
-  render(){
-const people=this.props.contacts;
-
-return <ol>  {
-    people.map(person=>(
-      <li key={person.name}>{person.name}</li>
-    )
-
-    )
-  }
-</ol>;
-
-
-  }
-}
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-       <ContactList contacts={[
-                      {name:'Joe'},
-                      {name:'Mary'},
-                      {name:'Neo'}
-                      ]}/>
-       <ContactList  contacts={[
-                      {name:'Amanda'},
-                      {name:'Barbie'},
-                      {name:'Peter'}
-                      ]}/>
-      </div>
-    );
+      return (
+            <div>
+              Hello World
+            </div>
+          );
   }
 }
 
