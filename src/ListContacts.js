095408f3405c64aaebf7bb869f2337de6562713a
Mary Bond <mbond1224@gmail.com>
2018-07-11 21:50:44 -0700

    EventHandlerDeleteButton for static version

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,32 +1,37 @@
-import React,{ Component } from 'react'
+import React, {Component} from 'react'
 
-class ListContacts extends Component{
-render(){
-   
-    return(
-    <ol className='contact-list'>
-   {
+class ListContacts extends Component {
+    render() {
 
-this.props.contacts.map(contact =>(
-<li key={contact.id}>
-{contact.name}
+        return (
+            <ol className='contact-list'>
+                {this
+                    .props
+                    .contacts
+                    .map(contact => (
+                        <li key={contact.id} className='contact-list-item'>
 
-</li>
+                            <div
+                                className='contact-avatar'
+                                style={{
+                                backgroundImage: `url(${contact.avatarURL})`
+                            }}/>
+                            <div className='contact-detail'>
+                                <p>{contact.name}</p>
+                                <p>{contact.email}</p>
+                            </div>
+                            <button onClick={() => this.props.onDeleteContact(contact)} className='contact-remove'>
+                                Remove
+                            </button>
+                        </li>
 
-)
-
-)
-
-
-   } 
-    
-    
-    
-    </ol>
-    )
-
+                    ))
 }
 
+            </ol>
+        )
+
+    }
 
 }
 export default ListContacts;
