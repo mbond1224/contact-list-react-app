d5ba902d2cab8e4bd761bd2fa3bbf19a66c34342
Mary Bond <mbond1224@gmail.com>
2018-07-12 00:42:09 -0700

    FetchContactsFunctionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import ListContacts from './ListContacts'
+import ListContacts from './ListContacts';
+import * as ContactsApi from './utils/ContactsAPI';
 /* constructor () {
   this.state = {
     contacts: []
@@ -9,26 +10,13 @@ import ListContacts from './ListContacts'
 class App extends Component {
 
   state = {
-    contacts: [
-      {
-        "id": "ryan",
-        "name": "Ryan Florence",
-        "email": "ryan@reacttraining.com",
-        "avatarURL": "http://localhost:5001/ryan.jpg"
-      },
-      {
-        "id": "michael",
-        "name": "Michael Jackson",
-        "email": "michael@reacttraining.com",
-        "avatarURL": "http://localhost:5001/michael.jpg"
-      },
-      {
-        "id": "tyler",
-        "name": "Tyler McGinnis",
-        "email": "tyler@reacttraining.com",
-        "avatarURL": "http://localhost:5001/tyler.jpg"
-      }
-    ]
+    contacts: []
+  }
+  componentDidMount(){
+    ContactsApi.getAll().then(
+      (contacts)=>{this.setState({contacts})}
+    );
+
   }
   removeContact = (contact) => {
        this.setState((state) => ({
