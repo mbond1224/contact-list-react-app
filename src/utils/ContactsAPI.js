8af74970ee9a2a43363e240cdfff24810357b2ee
Mary Bond <mbond1224@gmail.com>
2018-07-18 06:34:33 -0700

    Put heroku app link

--- src/utils/ContactsAPI.js
+++ src/utils/ContactsAPI.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://localhost:5001'
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'https://reactcontactserver.herokuapp.com/'
 
 let token = localStorage.token
 
